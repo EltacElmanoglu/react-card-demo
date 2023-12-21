712a966a29981fcc38a31c3dd73d83f0c1993799
Eltaj Osmanli <eltac2000@gmail.com>
2023-12-22 00:39:30 +0400

    first commit

---
+++ src/Data.js
@@ -0,0 +1,40 @@
+import angularImage from "./img/angular.jpg";
+import bootstrapImage from "./img/bootstrap5.png";
+import ccsharpImage from "./img/ccsharp.png";
+import compleWeb from "./img/kompleweb.jpg";
+
+const cardsData = [
+  {
+    id: 1,
+    image: angularImage,
+    title: "Angular",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus aspernatur labore cum hic neque. Veritatis facere cumque culpa eaque, iusto fugiat, natus nemo nam laborum illo tempore voluptas labore ratione!",
+  },
+
+  {
+    id: 2,
+    image: bootstrapImage,
+    title: "Bootstrap",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus aspernatur labore cum hic neque. Veritatis facere cumque culpa eaque, iusto fugiat, natus nemo nam laborum illo tempore voluptas labore ratione!",
+  },
+
+  {
+    id: 3,
+    image: ccsharpImage,
+    title: "Ccsharp",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus aspernatur labore cum hic neque. Veritatis facere cumque culpa eaque, iusto fugiat, natus nemo nam laborum illo tempore voluptas labore ratione!",
+  },
+
+  {
+    id: 4,
+    image: compleWeb,
+    title: "CompleWeb",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus aspernatur labore cum hic neque. Veritatis facere cumque culpa eaque, iusto fugiat, natus nemo nam laborum illo tempore voluptas labore ratione!",
+  },
+];
+
+export default cardsData;
